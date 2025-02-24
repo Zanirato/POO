ff472588bf0ccce57c2f9f90a963d6590033b933
Julia Zanirato <juliazaniratoo@gmail.com>
2025-02-24 15:36:51 -0300

    js

---
+++ objeto.js
@@ -0,0 +1,11 @@
+// Criando um objeto de aluno
+const aluno = {
+    nome: "Zani",
+    idade: 17,
+
+    estudar: () => console.log
+    (`${aluno.nome} está estudando.`)
+};
+
+console.log(aluno.nome);
+aluno.estudar();
